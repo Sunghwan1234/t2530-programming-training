753649a4583fd0a6a3346d0fa72d8674e6daaa91
Sunghwan1234 <sunghwan1002222@gmail.com>
2024-09-04 20:34:49 -0500

    LEDS UPDATE AFTER SO LONG!
    added a lot of comments and cleaned up stuff
    new Keys thingy that I will not use and will delete right away lmao
    reworking rainbow because it sucks: new vibrance thingy thats goofy aah

--- hello_world/src/hello/LEDs.java
+++ hello_world/src/hello/LEDs.java
@@ -12,15 +12,16 @@ import java.awt.Graphics;
 import java.util.Arrays;
 
 public class LEDs {
-    int NumberOfLeds = 50;
+    boolean key = false;
 
-    int[] LedBuffer = new int[NumberOfLeds*4*4/3];
+    int NumberOfLeds = 50; // Number of leds. change accordingly.
+    double timer = 0; // timer. any questions?
 
-    int[] RenderingLedData = new int[NumberOfLeds*4];
+    int[] RenderingLedData = new int[NumberOfLeds*4]; // LED RGBW. FORMAT: GRBW | Used by the Renderer
+    int[] LedBuffer = new int[NumberOfLeds*4*4/3]; // WPILIB. FORMAT: BGR | Used by WPILIB
 
-    int[] LedRGBWData = new int[NumberOfLeds*4];
-
-    double timer = 0;
+    // NEW CODE TO ADD
+    int[] LedRGBWData = new int[NumberOfLeds*4]; // ME! FORMAT: RGBW | Data to store the RGBW data before pushing to the LEDS.
 
     public LEDs() {}
 
@@ -33,7 +34,7 @@ public class LEDs {
     public void periodic() {
         timer += 1;
         //blink(50, 120, new Color(0,255,0),new Color(255,0,0));
-        //rainbow(10,0.05);
+        rainbow(0.1,0.1,0.5);
          // for (int i=0;i<12;i++) {
         //     if (i % 2 == 1) {
         //         setRGBW(i, 0, 1, 255, 3);
@@ -41,7 +42,7 @@ public class LEDs {
         //         setRGBW(i, 0, 255, 2, 3);
         //     }
         // }
-        for (int i=0;i<20;i++) {setDataRGBW(i,11,222,33,44);}
+        //for (int i=0;i<20;i++) {setDataRGBW(i,11,222,33,44);}
 
         // setRGB(0, 0, 255, 0);
         // setRGB(1, 255, 0, 0);
@@ -52,18 +53,23 @@ public class LEDs {
         //print(Arrays.toString(LedRGBWData));
         pushData();
     }
-    public void rainbow(int Speed, double Change) {
+    public void rainbow(double Speed, double Change, double Vibrance) {
         /** RAINBOW LIGHTS!!!
-         * Speed: Higher = lower speed of the rainbow I like 10~100
-         * Change: 0.0~1.0
+         * Speed: 0.0 ~ 1.0
+         * Change: 0.0 ~ 1.0
+         * Vibrance: 0.0 ~ 1.0 [TESTING]
+         * 
+         * rainbow: The current color the first LED.
+         * i: Which LED?
+         * j: Change. Since the change is inside of SIN, adding PI adds nothing
          */
-        double rainbow = timer/Speed;
+        double rainbow = timer*Speed/10;
         for (int i=0;i<NumberOfLeds-1;i++) {
             double j = i*(PI-Change);
             setDataRGBW(i,
             (int) (sin(rainbow+j)*255),
-            (int) (sin(rainbow+2+j)*255),
-            (int) (sin(rainbow+4+j)*255),0);}
+            (int) (sin(rainbow+(PI*Vibrance)+j)*255),
+            (int) (sin(rainbow+(PI*Vibrance*2)+j)*255),0);}
     }
     public void blink(int Percent, int Length, Color color1, Color color2) {
         int blinktime = (int) timer % Length;
@@ -74,22 +80,6 @@ public class LEDs {
                 setDataRGBW(i, color2.getRed(),color2.getGreen(),color2.getBlue(),0);
             }
         }
-    }
-
-    public void setRGB(int index, int R, int G, int B) { // CLASS FILE
-        LedBuffer[index*4] = B;
-        LedBuffer[(index*4)+1] = G;
-        LedBuffer[(index*4)+2] = R;
-        LedBuffer[(index*4)+3] = 0;
-    }
-    public Color getLED(int index) { // CLASS FILE
-        return new Color(
-            (int) ((LedBuffer[index * 4 + 2] & 0xFF) / 255.0),
-            (int) ((LedBuffer[index * 4 + 1] & 0xFF) / 255.0),
-            (int) ((LedBuffer[index * 4] & 0xFF) / 255.0));
-            // LedBuffer[index * 4 + 2],
-            // LedBuffer[index * 4 + 1],
-            // LedBuffer[index * 4]);
     }
 
     public int[] getDataRGBW(int index) {
@@ -105,7 +95,7 @@ public class LEDs {
         LedRGBWData[index * 4 + 2] = B;
         LedRGBWData[index * 4 + 3] = W;
     }
-    public void pushData() {
+    public void pushData() { // This sets the RGBW
         for (int i=0; i<NumberOfLeds-1;i++) {
             setRGBW(i, LedRGBWData[i * 4],
             LedRGBWData[i * 4 + 1],
@@ -115,7 +105,7 @@ public class LEDs {
     }
 
     public void setRGBW(int index, int R, int G, int B, int W) {
-        int offsetindex = index + (int) Math.floor(index/3); // Index offset ONLY FOR ENCODING INDO GRBGRBGRBGRB
+        int offsetindex = index + (int) Math.floor(index/3); // Index offset ONLY FOR ENCODING INTO GRBGRBGRBGRB
 
         int[] prevRGBW = getDataRGBW(index);
         if (index>0) {prevRGBW = getDataRGBW(index-1);}
@@ -124,13 +114,24 @@ public class LEDs {
 
         //print(prevRGB.toString());
         /** HOW TO READ R G B comments
-         * I will use C and V as example
+         * I will use C (Current) and V (Value) as example
          * C=C: Use C to output as C. basically just normal setRGB
-         * C=V: Use V to output as C. for example G=W (W is the input value from here)
+         * C=V: Use the V to output as C. for example G=W (W is the input value from here)
          * C=+V: Use the next index's V to output
          * C=-V: Use the previous index's V to output.
          * 
          * REMEMBER: you use the INDEX, not the OFFSETINDEX
+         * 
+         * How tf this encodes?
+         * WPIPID hates simplicity, so I have to make this!!!
+         * 
+         * YOUR input:      RGB W|RG BW|R GBW|RGBW|...
+         * input:           RGB|R GB|RG B|RGB|RGB|...
+         * 
+         * setLED:          BGR|B GR|BG R|BGR|BGR|...
+         *                    reorders correctly
+         * WPILIB sends:    GRB|G RB|GR B|GRB|GRB|...
+         * output:          GRB W|GR BW|G RBW|GRBW|...
          */
 
 
@@ -151,15 +152,31 @@ public class LEDs {
         }
     }
 
-    public void render(Graphics g) { // This replaces the actual LEDs for a virtual one. should work perfectly :D
-        for (int i=0;i<LedBuffer.length/4;i+=1) { // This converts the LedBuffer to the rendering data, which is GRBW.
+    public void setRGB(int index, int R, int G, int B) { // CLASS FILE | encodes in BGR, for some reason???
+        LedBuffer[index*4] = B;
+        LedBuffer[(index*4)+1] = G;
+        LedBuffer[(index*4)+2] = R;
+        LedBuffer[(index*4)+3] = 0; // I think its a seperator, maybe for the W, but never used and useless.
+    }
+    public Color getLED(int index) { // CLASS FILE | what is this lol you get led data (except the 0) in RGB format
+        return new Color(
+            (int) ((LedBuffer[index * 4 + 2] & 0xFF) / 255.0),
+            (int) ((LedBuffer[index * 4 + 1] & 0xFF) / 255.0),
+            (int) ((LedBuffer[index * 4] & 0xFF) / 255.0));
+            // LedBuffer[index * 4 + 2],
+            // LedBuffer[index * 4 + 1],
+            // LedBuffer[index * 4]);
+    }
+
+    public void render(Graphics g) { // VIRTUAL LED | This replaces the actual LEDs for a virtual one. should work perfectly :D
+        for (int i=0;i<LedBuffer.length/4;i+=1) { // This converts the LedBuffer to the rendering data, which is GRB. Done inside the CLASS
             RenderingLedData[i*3 + 1] = LedBuffer[i*4 + 2]; // G 
             RenderingLedData[i*3 + 0] = LedBuffer[i*4 + 1]; // R
             RenderingLedData[i*3 + 2] = LedBuffer[i*4 + 0]; // B
         }
         //System.out.println(Arrays.toString(LED));
 
-        for (int i=0;i<RenderingLedData.length/4;i+=1) { // The actual rendering :D
+        for (int i=0;i<RenderingLedData.length/4;i+=1) { // The actual rendering. This is the LEDS.
             int W = RenderingLedData[(i*4)+3]/2;
             Color RGBW = new Color(
                 min(abs( RenderingLedData[(i*4)+1]) + W, 255),
--- hello_world/src/hello/Renderer.java
+++ hello_world/src/hello/Renderer.java
@@ -7,16 +7,19 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.Timer;
 
-public class Renderer extends JPanel implements ActionListener {
+public class Renderer extends JPanel implements ActionListener, KeyListener {
     private final Timer timer;
     private final LEDs leds;
 
     public Renderer() {
         this.leds = new LEDs();
 
-        leds.run();
+        //leds.run();
 
         this.timer = new Timer(1, this);
         this.timer.start();
@@ -39,6 +42,29 @@ public class Renderer extends JPanel implements ActionListener {
 
         g.dispose();
     }
+
+    @Override
+    public void keyPressed(KeyEvent e) { // - - - - - - - - - - CONTROLS - - - - - - - - - - \\
+        // Left arrow: 37
+        switch(e.getKeyCode()) {
+            case 32: // Space bar
+                leds.key = true;
+                break;
+            default: break; 
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case 32: // Space bar
+                leds.key = false;
+                break;
+            default: break;
+        }
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {}
 
     @Override
     public void actionPerformed(ActionEvent e) { // timer thingy
